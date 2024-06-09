c8f74330a00ab1d6b2d37c593bacfc9eb1bb8700
GingerSchnitzel <141836349+GingerSchnitzel@users.noreply.github.com>
2024-06-09 17:56:19 +0300

    Merge conflict

--- CodeInPlace/FinalProject/FinalProject.py
+++ CodeInPlace/FinalProject/FinalProject.py
@@ -179,11 +179,8 @@ copy_button.grid(row=2, column=0, padx=(10), pady=(0, 14))  # Place the button b
 
 
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 68cf0bf (Update FinalProject.py)
 
 label_slider_frame.pack()
 label_checkbox_frame.pack()
